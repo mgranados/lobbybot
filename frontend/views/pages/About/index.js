9b1c54dd4d87ecd4b41df0a9050aecc7706220c1
Siedrix <siedrix@gmail.com>
2017-03-05 12:53:08 -0600

    Added about page

--- frontend/views/pages/About/index.js
+++ frontend/views/pages/About/index.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class About extends Component {
   render () {
     return (
-      <div>
-        About
+      <div className="single-page">
+        <h1>About</h1>
+        <p>This is a repo created to start projects by <a href="https://twitter.com/__arch3r">@__arch3r</a>, <a href="https://twitter.com/siedrix">@siedrix</a> and the peaplo at <a href="http://latteware.io/">Latteware.inc</a></p>
+        <p>Check the repo at <a href="https://github.com/latteware/koa-react-base">https://github.com/latteware/koa-react-base</a></p>
       </div>
     )
   }

3c1f33ad8b31951f540bffa409b0b437440e36d7
Siedrix <siedrix@gmail.com>
2017-04-15 16:05:13 -0500

    Added lib/task

--- server/databases/mongo.js
+++ server/databases/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const config = require('../../config/database')
 
-mongoose.Promise = Promise
+mongoose.Promise = global.Promise
 mongoose.connect(config.mongo.url)
 
 module.exports = mongoose.connection

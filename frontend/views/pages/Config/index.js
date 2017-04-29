95a795b067e52fa5e723f67c5c5b9129d243cb27
magiobus <aalbertobus@gmail.com>
2017-04-29 18:38:37 -0500

    Simple form on hotel config

--- frontend/views/pages/Config/index.js
+++ frontend/views/pages/Config/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import HotelForm from 'views/components/HotelForm'
+
 
 export default class Config extends Component {
   render () {
     return (
       <div>
         Configuración de Hotel
+        <br/>
+        <HotelForm></HotelForm>
       </div>
     )
   }

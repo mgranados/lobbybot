af751ec742fcce7e5d7e389bb2afa0e398189012
Martin Granados <margrangr@gmail.com>
2017-04-30 11:26:59 -0500

    foursquare list

--- server/api/hotels/places.js
+++ server/api/hotels/places.js
@@ -20,30 +20,53 @@ module.exports = {
             'payload': {
               'template_type': 'generic',
               'elements': [
+
+
+
+
                 {
-                  'title': 'Chiquito taller de cocina',
-                  'image_url': 'https://igx.4sqi.net/img/general/width960/8iNHOHJVOVUytPzfgXfefykAeRJNYmVBVMY-ABAYWKg.jpg',
-                  'subtitle': 'Restaurante Italiano',
-                  'buttons': [
+                      'title': 'Mina de Edén',
+                  'image_url': 'https://foursquare.com/v/mina-el-ed%C3%A9n/4c92791cf600236a7c5fbd32?openPhotoId=51eafeb5498ee2a4d355e5',
+                  'subtitle': 'Atraccion turistica',
+                  'buttons': ['https://foursquare.com/v/mina-el-ed%C3%A9n/4c92791cf600236a7c5fbd32'
                     {
                       'type': 'web_url',
-                      'url': 'https://foursquare.com/v/chiquito-taller-de-cocina/4e6bdbad7d8b2c711b07b643',
+                      'url': 'https://foursquare.com/v/mina-el-ed%C3%A9n/4c92791cf600236a7c5fbd32',
                       'title': 'Muéstrame más'
                     }
                   ]
                 },
                 {
-                  'title': 'Chiquito taller de cocina',
-                  'image_url': 'https://igx.4sqi.net/img/general/width960/8iNHOHJVOVUytPzfgXfefykAeRJNYmVBVMY-ABAYWKg.jpg',
-                  'subtitle': 'Restaurante Italiano',
-                  'buttons': [
+                  'title': 'Teleferico estación el grillo',
+                  'image_url': 'https://foursquare.com/v/teleferico-estacion-el-grillo/4bb3728d715eef3b1f5286bb?openPhotoId=4fca40c4e4b04ba0d0ecf0cc',
+                  'subtitle': 'atraccion turistica',
+                  'buttons': [ 'https://foursquare.com/v/teleferico-estacion-el-grillo/4bb3728d715eef3b1f5286bb'
                     {
                       'type': 'web_url',
-                      'url': 'https://foursquare.com/v/chiquito-taller-de-cocina/4e6bdbad7d8b2c711b07b643',
+                      'url': 'https://foursquare.com/v/teleferico-estacion-el-grillo/4bb3728d715eef3b1f5286bb',
                       'title': 'Muéstrame más'
                     }
                   ]
-                }
+              },{
+                'title': 'La Mina Club',
+                'image_url': 'https://foursquare.com/v/teleferico-estacion-el-grillo/4bb3728d715eef3b1f5286bb?openPhotoId=4fca40c4e4b04ba0d0ecf0cc',
+                'subtitle': 'Antro',
+                'buttons': [ 'https://foursquare.com/v/teleferico-estacion-el-grillo/4bb3728d715eef3b1f5286bb'
+                  {
+                    'type': 'web_url',
+                    'url': 'https://foursquare.com/v/la-mina-club/4bf760595ec320a1e40587d3',
+                    'title': 'Muéstrame más'
+                  }
+                ]
+              }
+
+
+
+
+
+
+
+
               ]
             }
           }
